bc5e6ba8890879206b8c5d848db990e457bfefda
caitevanish <cait.evan@gmail.com>
2022-02-28 08:41:15 -0800

    Added the getVideo async function to the App component. Created and gitignored the youttube API key. Tested get request in Postman successfully.

--- src/App.js
+++ src/App.js
@@ -3,42 +3,56 @@ import LoginForm from "./Components/LoginForm/LoginForm";
 import RegistrationForm from "./Components/RegistrationForm/RegistrationForm";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import jwt_decode from "jwt-decode";
 import Home from "./Components/Home/Home";
-
+import axios from "axios";
+import keys from "./API_keys.json";
 
 function App() {
-
   const [user, setUser] = useState(null);
 
   useEffect(() => {
+    const jwt = localStorage.getItem("token");
 
-    const jwt = localStorage.getItem('token');
-    
-    try{
+    try {
       const decodedUser = jwt_decode(jwt);
       setUser(decodedUser);
-    
-    } catch{}
-  }, [])
+    } catch {}
+  }, []);
 
+  async function getVideo(request) {
+    let response = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?q=letterkenny+stewart+roald&key=${keys.googleAPIkey}`
+    );
+    console.log(response);
+  }
+
+  // useEffect(() => {
+  //   getVideo();
+  // }, []);
 
   return (
     <div className="App">
       <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="home" element={() => {
-          if (!user) {
-            return <LoginForm />
-          }
-          else {
-            return <Home user={user} />
-          }
-        }} />
-        <Route path="loginform" element={<LoginForm user={user}/>} />
-        <Route exact path="loginform/registration" element={<RegistrationForm />} />
+        <Route
+          path="home"
+          element={() => {
+            if (!user) {
+              return <LoginForm />;
+            } else {
+              return <Home user={user} />;
+            }
+          }}
+        />
+        <Route path="loginform" element={<LoginForm user={user} />} />
+        <Route
+          exact
+          path="loginform/registration"
+          element={<RegistrationForm />}
+        />
       </Routes>
     </div>
   );
